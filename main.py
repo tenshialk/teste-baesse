ecc1baa500d5c63718f559c2fb2e0373a943d71a
tenshialk <mariacicera0981@gmail.com>
2025-08-08 23:39:19 +0000

    20:38

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-from flask import flask 
-from models import Usuario
-from db import db
+from flask import Flask, render_template, redirect, request, flash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db.init_app(app)
+app.config['SECRET_KEY'] = 'pizza'
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('login.html')
 
 
-@app.route('/registrar', methods=['GET', 'POST'])
-def registrar():
-    if request.method == 'GET':
-        return render_template('registrar.html')
-    if request.method == 'GET':
-    elif request.method == 'POST':
-        email  = request.form[emailform]
-        senha = request.form[senhaform]
+@app.route('/login', methods=['POST'])
+def login():
+    matricula = request.form.get('matricula')
+    senha = request.form.get('senha')
+
+    if matricula == 'igo' and senha == '123':
+        return render_template('usuario.html')
+    else:
+        flash('USUARIO INVALIDO')
         
-        novo_usuario = Usuario(email=email, senha=senha)
-        db.session.add(novo_usuario)
-        db.session.commit()
+        
+        return redirect('/')
 
-        return redirect(url_for('Url_for(''home)'))  # Redirect to login page after registration
-          
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Create database tables
-    app.run(debug=True)  # Run the Flask application
-
-
-
-    
+    app.run(debug=True)
